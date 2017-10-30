81df302d454fa9527983392f89257ddc4db3b0a3
Bobby00 <edwarobert6@gmai.com>
2017-10-30 16:33:14 +0300

    completed the header part

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+//Back End Logic
+//A function to count up the inputted number while changing the multiples of 3, 5 and 15 to ping, pong and pingpong respectively.
+function countUp(input) {
+    var total = [];
+    for (var index = 1; index <= input; index++) {
+        //test
+        //console.log(index)
+        if (index % 3 === 0 && index % 5 === 0) {
+            total.push("PINGPONG");
+        } else if (index % 5 === 0) {
+            total.push("PONG");
+        } else if (index % 3 === 0) {
+            total.push("PING");
+        } else {
+            total.push(index);
+        }
+    };
+    return total;
+};
+//A function to clear the input field after successful submission of a number.
+function clearForm(form) {
+    $(':input').not(':button, :submit').val('');
+};
+
+// Front End Logic
+$(document).ready(function () {
+    $("#userInput form").submit(function (event) {
+        event.preventDefault();
+        //Clear previously displayed output
+        $("ul").empty();
+        //Store the user input into a variable
+        var inputtedNumber = parseInt($("#inputNumber").val());
+        //Call the function countUp to work on the new variable inputtedNumber inside a new variable
+        var countedNumbers = countUp(inputtedNumber);
+        //Print out the processed numbers in list form.
+        for (var index = 0; index <= countedNumbers.length - 1; index++) {
+            $("ul#output").append("<li>" + countedNumbers[index] + "</li>");
+        };
+        //Clear the previously filled input by calling the function clearForm to empty out the input field
+        clearForm("input");
+    });
+});
